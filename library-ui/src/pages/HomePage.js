f8bf22e352d81c37f5018a025e4232211c0fa217
Alli <allisonglittle@gmail.com>
2021-10-24 20:58:52 -0700

    Added header in HomePage return statement

--- library-ui/src/pages/HomePage.js
+++ library-ui/src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function HomePage() {
-    <div>
-        <h1>Welcome to the Team 44 Community Library</h1>
-    </div>
+    return (
+        <div>
+            <h1>Welcome to the Team 44 Community Library</h1>
+        </div>
+    )
 };
 
 export default HomePage;
