025a990044c310ef7f6ba9ae20e21c7771d70e5e
Alli <allisonglittle@gmail.com>
2021-10-24 21:04:52 -0700

    Created initial book management web components

---
+++ library-ui/src/pages/BookManagement.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BookManagement() {
+    return (
+        <div>
+            <h1>Book and Inventory Management</h1>
+            <div>
+                <h3>Add Title to Library</h3>
+            </div>
+        </div>
+    );
+};
+
+export default BookManagement;
