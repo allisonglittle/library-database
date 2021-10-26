c8c3d625e1ec3e51d1be64884391ae2d07579891
Alli <allisonglittle@gmail.com>
2021-10-25 19:43:07 -0700

    Added form for new book titles

--- library-ui/src/pages/BookManagement.js
+++ library-ui/src/pages/BookManagement.js
@@ -6,6 +6,18 @@ function BookManagement() {
             <h1>Book and Inventory Management</h1>
             <div>
                 <h3>Add Title to Library</h3>
+                <form>
+                    <label>ISBN: 
+                        <input type="text" name="ISBN" />
+                    </label>
+                    <label>Title: 
+                        <input type="text" name="Title" />
+                    </label>
+                    <label>Author: 
+                        <input type="text" name="Author" />
+                    </label>
+
+                </form>
             </div>
         </div>
     );
