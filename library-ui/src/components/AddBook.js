b52156aa1e49c5123c7d66e8b0601d946700b326
Alli <allisonglittle@gmail.com>
2021-10-25 20:55:42 -0700

    Created component for adding book

---
+++ library-ui/src/components/AddBook.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AddBook () {
+    return (
+        <div>
+            <h3>Add Title to Library</h3>
+            <form>
+                <label>ISBN: 
+                    <input type="text" name="ISBN" />
+                </label>
+                <label>Title: 
+                    <input type="text" name="Title" />                   
+                </label>
+                <label>Author: 
+                    <input type="text" name="Author" />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+
+    );
+};
+
+export default AddBook;
--- library-ui/src/pages/BookManagement.js
+++ library-ui/src/pages/BookManagement.js
@@ -1,26 +1,13 @@
 import React from "react";
+import AddBook from "../components/AddBook";
 import BookList from "../components/BookList";
 
 function BookManagement() {
     return (
         <div>
             <h1>Book and Inventory Management</h1>
-            <div>
-                <h3>Add Title to Library</h3>
-                <form>
-                    <label>ISBN: 
-                        <input type="text" name="ISBN" />
-                    </label>
-                    <label>Title: 
-                        <input type="text" name="Title" />
-                    </label>
-                    <label>Author: 
-                        <input type="text" name="Author" />
-                    </label>
-                    <button type="submit">Submit</button>
-                </form>
-                <BookList />
-            </div>
+            <AddBook />
+            <BookList />
         </div>
     );
 };
