ffcc4189b36fc9769fac382e2ddf2d80ceedf885
Allison Little <littlea@flip3.engr.oregonstate.edu>
2021-10-28 20:34:53 -0700

    Added express app

---
+++ library-express/app.js
@@ -0,0 +1,28 @@
+// App.js
+
+/*
+    SETUP
+*/
+var express = require('express');   // We are using the express library for the web server
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = 4402;                 // Set a port number at the top so it's easy to change in the future
+
+/*
+    ROUTES
+*/
+app.get('/', function(req, res)                 
+    {
+        res.sendFile(__dirname + "/public/pages/homepage.html")      
+    });                                         
+
+app.get('/title_management', function(req, res)                 
+    {
+        res.sendFile(__dirname + "/public/pages/title_management.html")      
+    });                                         
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
