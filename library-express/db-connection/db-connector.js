0df56c24b9b48016851c87fc6e64ef92f549f3d7
ivyadam <adam.ivy@gmail.com>
2021-11-01 23:13:48 -0700

    add mysql connection

---
+++ library-express/db-connection/db-connector.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : 'cs340_[your_onid]',
+    password        : 'i8N[WmfYYwQTCFcT',
+    database        : 'cs340_ivya'
+});
+
+// Export it for use in our application
+module.exports.pool = pool;
