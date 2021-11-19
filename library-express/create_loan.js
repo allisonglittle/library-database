77d87b7f0d6adc8ba34bca567d77920f2a4b2543
Alli <allisonglittle@gmail.com>
2021-11-18 19:32:41 -0800

    Added patron select to loan page

--- library-express/create_loan.js
+++ library-express/create_loan.js
@@ -1,11 +1,11 @@
-module.exports = function(){
+module.exports = function () {
     var express = require('express');
     var router = express.Router();
 
     /*Get loan information from database*/
-    function getLoanDetails(res, mysql, context, complete){
-        mysql.pool.query("SELECT l.loanID, CONCAT(p.firstName, ' ', p.lastName) as 'patronName', DATE_FORMAT(l.loanDate, '%m/%d/%Y') as loanDate, count(li.bookID) as 'bookCount' FROM Loans l INNER JOIN Patrons p ON l.memberID = p.memberID INNER JOIN LoanItems li on l.loanID = li.loanID GROUP BY l.loanID", function(error, results, fields){
-            if(error){
+    function getLoanDetails(res, mysql, context, complete) {
+        mysql.pool.query("SELECT l.loanID, CONCAT(p.firstName, ' ', p.lastName) as 'patronName', DATE_FORMAT(l.loanDate, '%m/%d/%Y') as loanDate, count(li.bookID) as 'bookCount' FROM Loans l INNER JOIN Patrons p ON l.memberID = p.memberID INNER JOIN LoanItems li on l.loanID = li.loanID GROUP BY l.loanID", function (error, results, fields) {
+            if (error) {
                 res.write(JSON.stringify(error));
                 res.end();
             }
@@ -15,34 +15,44 @@ module.exports = function(){
     }
 
     /* Get Patron info from database */
-    
+    function getPatrons(res, mysql, context, complete) {
+        mysql.pool.query("SELECT p.memberID, p.firstName, p.lastName FROM Patrons p", function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.patrons = results;
+            complete();
+        });
+    }
 
     /*Display all loans.*/
-    router.get('/', function(req, res){
+    router.get('/', function (req, res) {
         var callbackCount = 0;
         var context = {};
         var mysql = req.app.get('mysql');
         getLoanDetails(res, mysql, context, complete);
-        function complete(){
+        getPatrons(res, mysql, context, complete);
+        function complete() {
             callbackCount++;
-            if(callbackCount >= 1){
+            if (callbackCount >= 2) {
                 res.render('create_loan', context);
             }
         }
-    }); 
+    });
 
     /*Create a new loan.*/
-    router.post('/', function(req, res){
+    router.post('/', function (req, res) {
         console.log(req.body)
         var mysql = req.app.get('mysql');
         var sql = "INSERT into Loans (memberID, loanDate) VALUES (?, CURRENT_DATE())";
         var inserts = [req.body.patronID];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
-            if(error){
+        sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
                 console.log(JSON.stringify(error))
                 res.write(JSON.stringify(error));
                 res.end();
-            }else{
+            } else {
                 res.redirect('/');
             }
         });
