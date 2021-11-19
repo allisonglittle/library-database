1fc38322471524d68554dd7874852e0e7c8d1fb7
Alli <allisonglittle@gmail.com>
2021-11-18 18:54:04 -0800

    Added search patron backend - no functionality

--- library-express/patrons.js
+++ library-express/patrons.js
@@ -39,6 +39,20 @@ module.exports = function () {
         }
     });
 
+    /* Display patrons filtered by first name */
+    function getPatronsByFirstName(req, res, mysql, context, complete) {
+        var query = "SELECT p.firstName, p.lastName, p.registerDate, p.contactEmail, p.contactPhone, t.bookTitle FROM Patrons p LEFT JOIN Titles t ON p.favoriteTitle = t.ISBN WHERE p.firstName LIKE '%?%'";
+        console.log(req.params)
+        var inserts = [req.params.filterFName]
+        mysql.pool.query(query, inserts, function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.patrons = results;
+            complete();
+        });
+    }
     /* Adds a patron, redirects to the patron page after adding */
     router.post('/', function (req, res) {
         console.log(req.body);
@@ -57,5 +71,19 @@ module.exports = function () {
 
     });
 
+    /*Display all patrons with first names like the given string */
+    router.get('/search/:s', function (req, res) {
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["searchpatrons.js"];
+        var mysql = req.app.get('mysql');
+        getPatronsWithNameLike(req, res, mysql, context, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 1) {
+                res.render('patrons', context);
+            }
+        }
+    });
     return router;
 }();
